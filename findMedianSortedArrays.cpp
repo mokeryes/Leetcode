157dda25d22916cca9c62bc278f04840ad72c911
xangle <lvthislv@gmail.com>
2022-10-24 04:12:53 +0800

    中位数

--- findMedianSortedArrays.cpp
+++ findMedianSortedArrays.cpp
@@ -13,20 +13,24 @@ public:
     /**
       * 暴力求解：时间复杂度取决于排序算法的时间复杂度（时间复杂度O(m+n)）
       */
+//    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+//        double median_num = 0.0;
+//
+//        for (int i = 0; i < nums2.size(); i++)
+//            nums1.push_back(nums2[i]);
+//
+//        sort(nums1.begin(), nums1.end());
+//
+//        const int length = nums1.size();
+//
+//        if (length % 2 == 0)
+//            return ((double)nums1[(length/2)-1] + (double)nums1[(length/2)]) / 2.0;
+//        else
+//            return (double)nums1[length/2.0];
+//    }
+
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        double median_num = 0.0;
-
-        for (int i = 0; i < nums2.size(); i++)
-            nums1.push_back(nums2[i]);
-
-        sort(nums1.begin(), nums1.end());
-
-        const int length = nums1.size();
-
-        if (length % 2 == 0)
-            return ((double)nums1[(length/2)-1] + (double)nums1[(length/2)]) / 2.0;
-        else
-            return (double)nums1[length/2.0];
+        return 1.0;
     }
 };
 
